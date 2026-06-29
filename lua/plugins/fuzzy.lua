1cb1413f2d36b37abdf1d5ac8b608c559514ca9c
João Pedro <irus0ubusiness@gmail.com>
2026-06-29 15:52:01 +0100

    feat: add ignore .git files from file_search in telescope

--- lua/plugins/fuzzy.lua
+++ lua/plugins/fuzzy.lua
@@ -5,6 +5,15 @@ return {
       "nvim-lua/plenary.nvim",
     },
     config = function()
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = {
+            "^%.git/",
+            "^%.git\\",
+          },
+        },
+      })
+
       local builtin = require("telescope.builtin")
 
       vim.keymap.set("n", "<leader>ff", builtin.find_files)
